12d3e44681ad7fd2e5e2f0ecb2a20f0a605a75e3
KeepGen <keeper.gen@gmail.com>
2022-03-13 21:59:52 +0200

    feat: added random figure module

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import { ClicksModule } from './modules/clicks.module';
 import { RandomPhotoModule } from './modules/show.random.photo.module';
 import { RandomSoundModule } from './modules/randomsound.module';
 import { CastomMsg } from './modules/castommsg.module';
+import { RandomFigureModule } from './modules/random.figure.module';
 import { TimerModule } from './modules/timer.module';
 
 let bgChange = new BackgroundModule('bgChange', 'Замена фона');
@@ -14,8 +15,9 @@ let clickModule = new ClicksModule('clickModule', 'Подсчет кликов')
 let showRandomPhoto = new RandomPhotoModule('showRandomPhoto', 'Показать рандомное фото');
 let randomSoundModule = new RandomSoundModule('randomSoundModule', 'Случайный звук');
 let сastomMsg = new CastomMsg('сastomMsg', 'Случайное сообщение');
+let randomFigureModule = new RandomFigureModule('randomFigureModule', 'Случайная фигура');
 let timerModule = new TimerModule('timerModule', 'Таймер 5 сек');
-const menuItems = [bgChange, clickModule, showRandomPhoto, randomSoundModule, сastomMsg, timerModule];
+const menuItems = [bgChange, clickModule, showRandomPhoto, randomSoundModule, сastomMsg, randomFigureModule, timerModule];
 
 const contextMenu = new ContextMenu('.menu');
 contextMenu.open();
